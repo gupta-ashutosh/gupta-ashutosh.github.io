c300d53ab74555693d05ba03fff4f65fdda4e4a3
ashutosh <ashutoshgupta2605@gmail.com>
2020-08-18 17:55:45 +0530

    changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,5 +35,5 @@ DEFAULT_PAGINATION = 10
 #RELATIVE_URLS = True
 MARKUP = ('md', 'ipynb')
 
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['ipynb.markup']
+# PLUGIN_PATHS = ['./plugins']
+# PLUGINS = ['ipynb.markup']
